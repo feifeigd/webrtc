b2139efb23ac8878557be3f998399d15c14bc1cf
feifeigd <feifeigd@21cn.com>
2016-12-17 00:12:42 +0800

    完成信令服务器

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ var WebSocketServer = require("ws").Server;
 
 wss = new WebSocketServer({port:8888});
 
+wss.on("listening", function(){
+    console.log("Server stared...");
+});
+
 wss.on("connection", function(connection){
     console.log("User connected.");
+    connection.send("Hello World.");
+
     connection.on("message", function(message){
         console.log("Got message:", message);
-    });
-    connection.send("Hello World.");
-    connection.on("message", function(message){
         var data;
         try{
             data = JSON.parse(message);
@@ -33,21 +36,38 @@ wss.on("connection", function(connection){
             sendTo(connection, {type:"login",success:true});
             break;
         case "offer":
-            var name = data.name;
+            var name = data.name;   // 被叫方
             console.log("Sending offer to ", name);
-            var conn = users[name];
+            var conn = users[name]; // 被叫方
             if(conn){
                 connection.otherName = name;
                 sendTo(conn, {type:"offer", offer:data.offer, name:connection.name});
             }
             break;
         case "answer":
-            var name = data.name;
+            var name = data.name;   // 呼叫方
             console.log("Sending answer to ", name);
-            var conn = users[name];
+            var conn = users[name]; // 呼叫方
             if(conn){
                 connection.otherName = name;
                 sendTo(conn, {type:"answer", answer:data.answer});
+            }
+            break;
+        case "candidate":
+            var name = data.name;
+            console.log("Sending candidate to ", name);
+            var conn = users[name];
+            if(conn){
+                sendTo(conn, {type:"candidate", candidate:data.candidate});
+            }
+            break;
+        case "leave":
+            var name = data.name;
+            console.log("Disconnecting user from ", name);
+            var conn = users[name];
+            if(conn){
+                conn.otherName = null;  // 挂断对方
+                sendTo(conn, {type:"leave"});
             }
             break;
         default:
@@ -62,6 +82,15 @@ wss.on("connection", function(connection){
         var name = connection.name;
         if(name){
             delete users[name];
+            name = connection.otherName;
+            if(name){
+                console.log("Disconnecting user from ", name);
+                var conn = users[name];
+                if(conn){
+                    conn.otherName = null;
+                    sendTo(conn, {type:"leave"});
+                }
+            }
         }
     })
 });
